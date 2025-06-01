486d769e19c756d9fee4947a9f820341d7b38004
RodrigoBG1 <rodribogu@gmail.com>
2025-06-02 00:09:26 +0200

    Deploy changes

--- backend/index.js
+++ backend/index.js
@@ -344,6 +344,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
 });
